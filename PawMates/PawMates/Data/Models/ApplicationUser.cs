ea64c17a74c3a84e630e88aaf536d3c69883732e
Zhan <zhanenev6@gmail.com>
2024-03-06 21:21:27 +0200

    Create ApplicationUser table
    ApplicationUser class extends IdentityUser class and add custom properties to IdentityUser

--- PawMates/PawMates/Data/Models/ApplicationUser.cs
+++ PawMates/PawMates/Data/Models/ApplicationUser.cs
@@ -1,35 +1,40 @@
-﻿using PawMates.Data.Enums;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PawMates.Data.Enums;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static PawMates.Data.DataConstants;
 
 namespace PawMates.Data.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
-        [Key]
-        public int Id { get; set; }
-
         [Required]
         [MaxLength(UserNamesMaxLenght)]
+        [Comment("User's first name")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(UserNamesMaxLenght)]
+        [Comment("User's last name")]
         public string LastName { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(UserCountryNameMaxLenght)]
+        [Comment("User's countrty name")]
         public string Country { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(UserAdressNameMaxLenght)]
+        [Comment("User's adress name")]
         public string Adress { get; set; } = string.Empty;
 
         [Required]
         [MaxLength(UserPhoneMaxLenght)]
+        [Comment("User's phone number")]
         public string MobileNumber { get; set; } = string.Empty;
 
+        [Comment("User's gender")]
         public Gender Gender { get; set; }
 
         public int PetId { get; set; }
