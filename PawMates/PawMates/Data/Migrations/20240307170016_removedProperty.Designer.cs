99625c629fc3faa1fe858d50d6f0513f5d9cd54f
Zhan <zhanenev6@gmail.com>
2024-03-07 19:06:46 +0200

    Removed MobileNumber
    Removed property from database

--- PawMates/PawMates/Data/Migrations/20240307170016_removedProperty.Designer.cs
+++ PawMates/PawMates/Data/Migrations/20240307170016_removedProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PawMates.Data;
 
@@ -11,9 +12,10 @@ using PawMates.Data;
 namespace PawMates.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240307170016_removedProperty")]
+    partial class removedProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -263,6 +265,12 @@ namespace PawMates.Data.Migrations
 
                     b.Property<DateTimeOffset?>("LockoutEnd")
                         .HasColumnType("datetimeoffset");
+
+                    b.Property<string>("MobileNumber")
+                        .IsRequired()
+                        .HasMaxLength(15)
+                        .HasColumnType("nvarchar(15)")
+                        .HasComment("User's phone number");
 
                     b.Property<string>("NormalizedEmail")
                         .HasMaxLength(256)
--- PawMates/PawMates/Data/Migrations/20240307170525_removedProperty2.Designer.cs
+++ PawMates/PawMates/Data/Migrations/20240307170525_removedProperty2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PawMates.Data;
 
@@ -11,9 +12,10 @@ using PawMates.Data;
 namespace PawMates.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240307170525_removedProperty2")]
+    partial class removedProperty2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
