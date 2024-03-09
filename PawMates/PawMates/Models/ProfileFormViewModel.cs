de7bb7dcf9aa1f418b0631e5be97541758d8b8d2
Zhan <zhanenev6@gmail.com>
2024-03-09 12:47:52 +0200

    Create ProfileFormViewModel.cs
    With properties and validation

---
+++ PawMates/PawMates/Models/ProfileFormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PawMates.Data.Enums;
+using PawMates.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static PawMates.Data.DataConstants;
+
+namespace PawMates.Models
+{
+    public class ProfileFormViewModel
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(OwnerNamesMaxLenght, MinimumLength = OwnerNamesMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(OwnerNamesMaxLenght, MinimumLength = OwnerNamesMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(OwnerCountryNameMaxLenght, MinimumLength = OwnerCountryNameMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string Country { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(OwnerAdressNameMaxLenght, MinimumLength = OwnerAdressNameMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string Adress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public Gender Gender { get; set; }
+
+        public ICollection<Pet> Pets { get; set; } = new List<Pet>();
+    }
+}
