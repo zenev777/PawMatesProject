45780cbf99bd104a7465cafcefbabc27ada13025
Zhan <zhanenev6@gmail.com>
2024-03-09 14:32:03 +0200

    Create PetController and Add view

---
+++ PawMates/PawMates/Controllers/PetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PawMates.Controllers
+{
+    public class PetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
