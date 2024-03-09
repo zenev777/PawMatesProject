6f58c91b2ec40834f6842697c69fd8685069d359
Zhan <zhanenev6@gmail.com>
2024-03-09 12:46:35 +0200

    new constants
    3 new constants for errors

--- PawMates/PawMates/Data/DataConstants.cs
+++ PawMates/PawMates/Data/DataConstants.cs
@@ -20,16 +20,16 @@
         public const int PetWeightMaxLenght = 100;
         public const int PetWeightMinLenght = 0;
 
-        //Users constants
+        //Owner constants
 
-        public const int UserNamesMaxLenght = 50;
-        public const int UserNamesMinLenght = 2;
-
-        public const int UserCountryNameMaxLenght = 56;
-        public const int UserCountryNameMinLenght = 4;
-
-        public const int UserAdressNameMaxLenght = 100;
-        public const int UserAdressNameMinLenght = 15;
+        public const int OwnerNamesMaxLenght = 50;
+        public const int OwnerNamesMinLenght = 2;
+                         
+        public const int OwnerCountryNameMaxLenght = 56;
+        public const int OwnerCountryNameMinLenght = 4;
+                         
+        public const int OwnerAdressNameMaxLenght = 100;
+        public const int OwnerAdressNameMinLenght = 15;
 
         //Events constants
 
@@ -51,5 +51,12 @@
 
         public const int TypeDescriptionMaxLenght = 250;
         public const int TypeDescriptionMinLenght = 20;
+
+        //Error message
+
+        public const string RequireErrorMessage = "The field {0} is required";
+        public const string StringLengthErrorMessage = "The field {0} must be between {2} and {1} characters long";
+        public const string RangeIntErrorMessage = "The field {0} must be between {1} and {2} characters long";
+
     }
 }
