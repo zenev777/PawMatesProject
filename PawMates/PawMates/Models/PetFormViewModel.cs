5be93a75b120eb1df0c17e6ca328759f041dcb18
zenev777 <117388163+zenev777@users.noreply.github.com>
2024-03-13 16:42:58 +0200

    PetFormViewModel created
    All properties added

---
+++ PawMates/PawMates/Models/PetFormViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PawMates.Data.Enums;
+using PawMates.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static PawMates.Data.DataConstants;
+
+namespace PawMates.Models
+{
+    public class PetFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PetNameMaxLenght, MinimumLength = PetNameMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [Range(PetAgeMinLenght, PetAgeMaxLenght, ErrorMessage =RequireErrorMessage)]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string DateOfBirth { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PetBreedMaxLenght, MinimumLength = PetBreedMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string Breed { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PetColorMaxLenght, MinimumLength = PetColorMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string MainColor { get; set; } = string.Empty;
+
+        [StringLength(PetColorMaxLenght, MinimumLength = PetColorMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string SecondaryColor { get; set; } = string.Empty;
+
+        [Required]
+        public Gender Gender { get; set; } 
+
+        [Required]
+        [Range(PetWeightMinLenght, PetWeightMaxLenght, ErrorMessage = RangeIntErrorMessage)]
+        public double Weight { get; set; }
+
+        public int PetTypeId { get; set; }
+
+        public ICollection<PetTypesViewModel> PetTypes { get; set;} = new List<PetTypesViewModel>();
+    }
+}
