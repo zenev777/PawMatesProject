e81a913af3671a135c29419aa9f46e6f5f33f0d1
zenev777 <117388163+zenev777@users.noreply.github.com>
2024-03-13 16:43:30 +0200

    PetTypesViewModel created

---
+++ PawMates/PawMates/Models/PetTypesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PawMates.Models
+{
+    public class PetTypesViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
