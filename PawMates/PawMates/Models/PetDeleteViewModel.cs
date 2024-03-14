f79cb23537bce90b39a1c6e54187471f1d666a88
Zhan <zhanenev6@gmail.com>
2024-03-14 18:46:15 +0200

    Create PetDeleteViewModel.cs

---
+++ PawMates/PawMates/Models/PetDeleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PawMates.Models
+{
+    public class PetDeleteViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
