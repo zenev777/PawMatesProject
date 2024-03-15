24e7379f79892e7843675fa5c3ef560c8d21900c
Zhan <zhanenev6@gmail.com>
2024-03-15 12:43:41 +0200

    Create EventInfoViewModel.cs

---
+++ PawMates/PawMates/Models/EventInfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PawMates.Models
+{
+    public class EventInfoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public string StartsOn { get; set; } = string.Empty;
+
+        public string OrganiserId { get; set; } = string.Empty;
+    }
+}
