96ded6a4db248a1c9522656f5d7633b4436262b7
Zhan <zhanenev6@gmail.com>
2024-03-15 12:12:24 +0200

    Update EventController.cs
    HttpPost Add method

--- PawMates/PawMates/Controllers/EventController.cs
+++ PawMates/PawMates/Controllers/EventController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PawMates.Data;
+using PawMates.Data.Models;
 using PawMates.Models;
+using System.Globalization;
+using System.Security.Claims;
+using static PawMates.Data.DataConstants;
 
 namespace PawMates.Controllers
 {
@@ -20,6 +24,48 @@ namespace PawMates.Controllers
             var model = new EventFormViewModel();
 
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(EventFormViewModel model)
+        {
+            DateTime start = DateTime.Now;
+
+            if (!DateTime.TryParseExact(
+                model.StartsOn,
+                EventStartDateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out start))
+            {
+                ModelState
+                    .AddModelError(nameof(model.StartsOn), $"Invalid date! Format must be: {EventStartDateFormat}");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var entity = new Event()
+            {
+                StartsOn = DateTime.Now,
+                Description = model.Description,
+                Name = model.Name,
+                OrganiserId = GetUserId(),
+            };
+
+            await data.Events.AddAsync(entity);
+
+            await data.SaveChangesAsync();
+
+            return RedirectToAction(nameof(All));
+        }
+
+
+        private string GetUserId()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
         }
     }
 }
