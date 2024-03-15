3c5955e1fc6f5d57a695fa7da792a8fc50522152
Zhan <zhanenev6@gmail.com>
2024-03-15 12:07:16 +0200

    Create EventController.cs
    Add HttpGet Add method

---
+++ PawMates/PawMates/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawMates.Data;
+using PawMates.Models;
+
+namespace PawMates.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly ApplicationDbContext data;
+
+        public EventController(ApplicationDbContext context)
+        {
+            data = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new EventFormViewModel();
+
+            return View(model);
+        }
+    }
+}
