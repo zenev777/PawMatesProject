f8687fb490656db87b3beb512cfdc75d84a8d859
Zhan <zhanenev6@gmail.com>
2024-03-15 12:43:27 +0200

    Update database
    Edit properties to nullable : ImageUrl and Description

--- PawMates/PawMates/Data/Migrations/20240315102827_updateDatabase.Designer.cs
+++ PawMates/PawMates/Data/Migrations/20240315102827_updateDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PawMates.Data;
 
@@ -11,9 +12,10 @@ using PawMates.Data;
 namespace PawMates.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240315102827_updateDatabase")]
+    partial class updateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
