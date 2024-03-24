8632e118193cbf3d9eca4def5b99610c912970e8
Zhan <zhanenev6@gmail.com>
2024-03-25 00:07:53 +0200

    Update EventController.cs
    Import service

--- PawMates/PawMates/Controllers/EventController.cs
+++ PawMates/PawMates/Controllers/EventController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PawMates.Core.Contracts.EventInterface;
+using PawMates.Core.Models.EventViewModels;
+using PawMates.Core.Models.PetViewModels;
+using PawMates.Core.Services.EventService;
+using PawMates.Extensions;
 using PawMates.Infrastructure.Data;
 using PawMates.Infrastructure.Data.Models;
-using PawMates.Models;
 using System.Globalization;
 using System.Security.Claims;
 using static PawMates.Infrastructure.Data.DataConstants;
@@ -14,29 +18,20 @@ namespace PawMates.Controllers
     public class EventController : Controller
     {
         private readonly ApplicationDbContext data;
+        private readonly IEventService eventService;
 
-        public EventController(ApplicationDbContext context)
+        public EventController(ApplicationDbContext context, IEventService _eventService)
         {
             data = context;
+            eventService = _eventService;
         }
 
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var events = await data.Events
-                .AsNoTracking()
-                .Select(e => new EventInfoViewModel()
-                {
-                    Name = e.Name,
-                    StartsOn = e.StartsOn.ToString(EventStartDateFormat),
-                    Location = e.Location,
-                    Description = e.Description,
-                    OrganiserId = e.Organiser.UserName,
-                    Id = e.Id,
-                })
-                .ToListAsync();
+            var model = await eventService.GetAllEventsAsync();
 
-            return View(events);
+            return View(model);
         }
 
         [HttpGet]
@@ -50,36 +45,20 @@ namespace PawMates.Controllers
         [HttpPost]
         public async Task<IActionResult> Add(EventFormViewModel model)
         {
-            DateTime start = DateTime.Now;
 
-            if (!DateTime.TryParseExact(
-                model.StartsOn,
-                EventStartDateFormat,
-                CultureInfo.InvariantCulture,
-                DateTimeStyles.None,
-                out start))
+            var userId = User.Id();
+
+            var result = await eventService.CreateEventAsync(model, userId);
+
+            if (result == false)
             {
-                ModelState
-                    .AddModelError(nameof(model.StartsOn), $"Invalid date! Format must be: {EventStartDateFormat}");
+                ModelState.AddModelError(nameof(model.StartsOn), $"Invalid date! Format must be: {EventStartDateFormat}");
             }
 
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
-
-            var entity = new Event()
-            {
-                StartsOn = start,
-                Description = model.Description,
-                Location = model.Location,
-                Name = model.Name,
-                OrganiserId = GetUserId(),
-            };
-
-            await data.Events.AddAsync(entity);
-
-            await data.SaveChangesAsync();
 
             return RedirectToAction(nameof(All));
         }
@@ -248,7 +227,7 @@ namespace PawMates.Controllers
                 return BadRequest();
             }
             
-            var ep = ev.EventParticipants
+            var ep = data.EventParticipants
                 .Where(ep => ep.EventId == model.Id)
                 .ToList();
 
