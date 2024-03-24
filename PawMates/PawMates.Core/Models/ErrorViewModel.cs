98586ddc07f109b39f1fe7a182adee9542699471
Zhan <zhanenev6@gmail.com>
2024-03-24 23:22:59 +0200

    Create ErrorViewModel.cs

---
+++ PawMates/PawMates.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PawMates.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
