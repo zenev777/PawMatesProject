6b8ce9180a7a1523060d801007d9459a4f11344c
Zhan <zhanenev6@gmail.com>
2024-03-24 23:23:15 +0200

    Create PetInfoViewModel.cs

---
+++ PawMates/PawMates.Core/Models/PetViewModels/PetInfoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PawMates.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace PawMates.Core.Models.PetViewModels
+{
+    public class PetInfoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string DateOfBirth { get; set; } = string.Empty;
+
+        public string Breed { get; set; } = string.Empty;
+
+        public string MainColor { get; set; } = string.Empty;
+
+        public string? SecondaryColor { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public double Weight { get; set; }
+
+        public string PetType { get; set; } = string.Empty;
+
+        public string OwnerId { get; set; } = null!;
+
+    }
+}
