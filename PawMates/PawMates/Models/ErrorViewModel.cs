0662151826810af8164663fc5e4ee463daa2903c
Zhan <zhanenev6@gmail.com>
2024-03-24 23:24:35 +0200

    Change folder

--- PawMates/PawMates/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PawMates.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- PawMates/PawMates/Models/EventDeleteViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PawMates.Models
-{
-    public class EventDeleteViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public DateTime StartsOn { get; set; }
-    }
-}
