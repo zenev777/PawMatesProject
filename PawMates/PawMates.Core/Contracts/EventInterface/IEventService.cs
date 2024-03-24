fa794f0c77f769860d7f8104dfce0c01c22b7e3c
Zhan <zhanenev6@gmail.com>
2024-03-24 23:22:56 +0200

    Create IEventService.cs

---
+++ PawMates/PawMates.Core/Contracts/EventInterface/IEventService.cs
@@ -0,0 +1,18 @@
+﻿using PawMates.Core.Models.EventViewModels;
+using PawMates.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.Core.Contracts.EventInterface
+{
+    public interface IEventService
+    {
+
+        Task<IEnumerable<EventInfoViewModel>> GetAllEventsAsync();
+
+
+    }
+}
