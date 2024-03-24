0f515031645555ce4618619db1043d3aaa6c3b99
Zhan <zhanenev6@gmail.com>
2024-03-25 00:07:36 +0200

    Update EventService.cs

--- PawMates/PawMates.Core/Services/EventService/EventService.cs
+++ PawMates/PawMates.Core/Services/EventService/EventService.cs
@@ -5,6 +5,7 @@ using PawMates.Infrastructure.Data.Common;
 using PawMates.Infrastructure.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,48 @@ namespace PawMates.Core.Services.EventService
             repository = _repository;
         }
 
+        public async Task<bool> CreateAsync(EventFormViewModel model, string userId)
+        {
+            if (await repository.AlreadyExistAsync<Event>(e=>e.Name == model.Name)) throw new ApplicationException("Event already exists");
+
+            DateTime start = DateTime.Now;
+
+            if (!DateTime.TryParseExact(
+                model.StartsOn,
+                EventStartDateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out start))
+            {
+                return false;
+            } 
+
+            var entity = new Event()
+            {
+                StartsOn = start,
+                Description = model.Description,
+                Location = model.Location,
+                Name = model.Name,
+                OrganiserId = userId,
+            };
+
+            await repository.AddAsync(entity);
+            await repository.SaveChangesAsync();
+
+            return true;
+
+        }
+
+        public Task<bool> CreateEventAsync(EventFormViewModel model, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditEventAsync()
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<IEnumerable<EventInfoViewModel>> GetAllEventsAsync()
         {
             return  await repository
@@ -28,7 +71,7 @@ namespace PawMates.Core.Services.EventService
                .Select(e => new EventInfoViewModel()
                {
                    Name = e.Name,
-                   StartsOn = e.StartsOn.ToString(EventStartDateFormat),
+                   StartsOn = e.StartsOn.ToString(EventStartDateFormat, CultureInfo.InvariantCulture),
                    Location = e.Location,
                    Description = e.Description,
                    OrganiserId = e.Organiser.UserName,
@@ -36,5 +79,7 @@ namespace PawMates.Core.Services.EventService
                })
                .ToListAsync();
         }
+
+
     }
 }
