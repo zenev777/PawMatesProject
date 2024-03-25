887490842e16c2538ac2ae05283353d6d3390686
Zhan <zhanenev6@gmail.com>
2024-03-25 14:37:55 +0200

    Update EventService.cs
    Add services

--- PawMates/PawMates.Core/Services/EventService/EventService.cs
+++ PawMates/PawMates.Core/Services/EventService/EventService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PawMates.Core.Contracts.EventInterface;
 using PawMates.Core.Models.EventViewModels;
+using PawMates.Infrastructure.Data;
 using PawMates.Infrastructure.Data.Common;
 using PawMates.Infrastructure.Data.Models;
 using System;
@@ -22,7 +23,7 @@ namespace PawMates.Core.Services.EventService
             repository = _repository;
         }
 
-        public async Task<bool> CreateAsync(EventFormViewModel model, string userId)
+        public async Task<bool> CreateEventAsync(EventFormViewModel model, string userId)
         {
             if (await repository.AlreadyExistAsync<Event>(e=>e.Name == model.Name)) throw new ApplicationException("Event already exists");
 
@@ -54,14 +55,30 @@ namespace PawMates.Core.Services.EventService
 
         }
 
-        public Task<bool> CreateEventAsync(EventFormViewModel model, string userId)
+        public async Task<int> EditEventAsync(int eventId, EventFormViewModel model)
         {
-            throw new NotImplementedException();
-        }
+                var eventToEdit = await repository.GetByIdAsync<Event>(eventId);
 
-        public Task<bool> EditEventAsync()
-        {
-            throw new NotImplementedException();
+                DateTime start = DateTime.Now;
+
+                if (!DateTime.TryParseExact(model.StartsOn,
+                EventStartDateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out start))
+                {
+                    return -1;
+                }
+
+                eventToEdit.Name = model.Name;
+                eventToEdit.Location = model.Location;
+                eventToEdit.StartsOn = start;
+                eventToEdit.Description = model.Description;
+                eventToEdit.Id = model.Id;
+
+                await repository.SaveChangesAsync();
+
+                return eventToEdit.Id;
         }
 
         public async Task<IEnumerable<EventInfoViewModel>> GetAllEventsAsync()
@@ -80,6 +97,40 @@ namespace PawMates.Core.Services.EventService
                .ToListAsync();
         }
 
+        public async Task<Event> EventByIdAsync(int id)
+        {
+            return await repository.AllReadOnly<Event>()
+                .Where(e => e.Id == id).FirstAsync();
+        }
 
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await repository.AllReadOnly<Event>().AnyAsync(e => e.Id == id);
+        }
+
+        public async Task<bool> SameOrganiserAsync(int eventId, string currentUserId)
+        {
+            bool result = false;
+            var Event = await repository.AllReadOnly<Event>()
+                .Where(e => e.Id == eventId)
+                .FirstOrDefaultAsync();
+
+            if (Event?.OrganiserId == currentUserId)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public async Task DeleteAsync(int eventId)
+        {
+            var eventToDelete = await repository.GetByIdAsync<Event>(eventId);
+            var joinedUsers = repository.All<EventParticipant>(ep => ep.EventId == eventId);
+            repository.DeleteRange(joinedUsers);
+            repository.Delete(eventToDelete);
+
+            await repository.SaveChangesAsync();
+        }
     }
 }
