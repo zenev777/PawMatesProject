0fa90a334f86a52576431a99d359f5e0551cfe88
Ivanelaa <zhanenev6@gmail.com>
2024-03-27 18:02:31 +0200

    Create Status.cs
    Enums for status

---
+++ PawMates/PawMates.Infrastructure/Data/Enums/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.Infrastructure.Data.Enums
+{
+    public enum Status
+    {
+        ForAdoption,
+        ForSale,
+        Lost,
+        ForWalking,
+        HappyPet
+    }
+}
