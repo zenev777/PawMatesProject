705f1b3327222fbebc40ba6a1e5136400916fd73
Zhan <zhanenev6@gmail.com>
2024-04-04 18:58:50 +0300

    Create PostController.cs
    Create HttpGet method All

---
+++ PawMates/PawMates/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawMates.Core.Contracts.EventInterface;
+using PawMates.Core.Contracts.PostInterface;
+using PawMates.Core.Services.EventService;
+
+namespace PawMates.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService postService;
+
+        public PostController(IPostService _postService)
+        {
+            postService = _postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await postService.GetAllPostsAsync();
+
+            return View(model);
+        }
+    }
+}
