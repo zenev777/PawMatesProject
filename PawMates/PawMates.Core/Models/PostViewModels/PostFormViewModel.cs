ab876bb25bc2e596703c2979201baa715599ba68
Zhan <zhanenev6@gmail.com>
2024-04-04 18:56:57 +0300

    Create PostViewModels

---
+++ PawMates/PawMates.Core/Models/PostViewModels/PostFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PawMates.Infrastructure.Data.DataConstants;
+
+namespace PawMates.Core.Models.PostViewModels
+{
+    public class PostFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PostDescriptionMaxLenght, MinimumLength = PostDescriptionMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string Creator { get; set; } = string.Empty;
+    }
+}
