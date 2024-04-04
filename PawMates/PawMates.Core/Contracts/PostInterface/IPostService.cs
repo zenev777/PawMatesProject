2ca5dcd16dcfa66f550b3c12ae08c5423048451d
Zhan <zhanenev6@gmail.com>
2024-04-04 18:56:30 +0300

    Create IPostService.cs

---
+++ PawMates/PawMates.Core/Contracts/PostInterface/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using PawMates.Core.Models.EventViewModels;
+using PawMates.Core.Models.PostViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.Core.Contracts.PostInterface
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<PostViewInfoModel>> GetAllPostsAsync();
+
+        Task<bool> CreatePostAsync(PostFormViewModel model, string id);
+    }
+}
