78e1038a2d98965b03195c0a25e98a456ca08bf8
Zhan <zhanenev6@gmail.com>
2024-04-04 18:57:59 +0300

    new mig
    delete property name from database - Posts

--- PawMates/PawMates.Infrastructure/Data/Migrations/20240404151011_DelName.Designer.cs
+++ PawMates/PawMates.Infrastructure/Data/Migrations/20240404151011_DelName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PawMates.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using PawMates.Infrastructure.Data;
 namespace PawMates.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240404151011_DelName")]
+    partial class DelName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
