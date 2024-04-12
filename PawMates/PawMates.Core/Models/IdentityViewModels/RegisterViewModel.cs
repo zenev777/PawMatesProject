cdcad1d7bfa9d515b39cf3c6aa279129a6d572b5
Zhan <zhanenev6@gmail.com>
2024-04-12 16:58:26 +0300

    Update RegisterViewModel.cs
    Add validations

--- PawMates/PawMates.Core/Models/IdentityViewModels/RegisterViewModel.cs
+++ PawMates/PawMates.Core/Models/IdentityViewModels/RegisterViewModel.cs
@@ -5,22 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static PawMates.Infrastructure.Data.DataConstants.IdentityConstants;
+using static PawMates.Infrastructure.Data.DataConstants.DataConstants;
 
 namespace PawMates.Core.Models.IdentityViewModels
 {
     public class RegisterViewModel
     {
         [Required]
-        [StringLength(UserUserameMaxLenght, MinimumLength = UserUserameMinLenght)]
+        [StringLength(UserUserameMaxLenght, MinimumLength = UserUserameMinLenght, ErrorMessage = StringLengthErrorMessage)]
         public string UserName { get; set; } = null!;
+
+        [StringLength(UserNamesMaxLenght, MinimumLength = UserNamesMinLenght,ErrorMessage = StringLengthErrorMessage)]
+        public string? FirstName { get; set; } = null!;
+
+        [StringLength(UserNamesMaxLenght, MinimumLength = UserNamesMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string? LastName { get; set; } = null!;
+
+        [StringLength(UserPhoneNumMaxLenght, MinimumLength = UserPhoneNumMinLenght, ErrorMessage = StringLengthErrorMessage)]
+        public string? PhoneNumber { get; set; } = null!;
 
         [Required]
         [EmailAddress]
-        [StringLength(UserEmailMaxLenght, MinimumLength = UserEmailMinLenght)]
+        [StringLength(UserEmailMaxLenght, MinimumLength = UserEmailMinLenght, ErrorMessage = StringLengthErrorMessage)]
         public string Email { get; set; } = null!;
 
         [Required]
-        [StringLength(UserPasswordMaxLenght, MinimumLength = UserPasswordMinLenght)]
+        [StringLength(UserPasswordMaxLenght, MinimumLength = UserPasswordMinLenght, ErrorMessage = StringLengthErrorMessage)]
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
 
