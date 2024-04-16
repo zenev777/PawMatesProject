f1fd395106371fb9ab5eb5561f3725b18b8632c1
Zhan <zhanenev6@gmail.com>
2024-04-16 14:34:23 +0300

    Create Area Contollers

---
+++ PawMates/PawMates/Areas/AdministrativeDirector/Controllers/AdministrativeDirectorBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PawMates.Areas.AdministrativeDirector.Controllers
+{
+    [Area("AdministrativeDirector")]
+    [Authorize("AdministrativeDirector")]
+    public class AdministrativeDirectorBaseController : Controller
+    { 
+    }
+}
