095a4fe47d79e3418e7369e024ddc4257511b17d
Zhan <zhanenev6@gmail.com>
2024-04-16 14:37:49 +0300

    Update UserContoller.cs
    Remove role methods

--- PawMates/PawMates/Controllers/UserContoller.cs
+++ PawMates/PawMates/Controllers/UserContoller.cs
@@ -112,6 +112,10 @@ namespace PawMates.Controllers
 
                 if (result.Succeeded)
                 {
+                    if (await userManager.IsInRoleAsync(user, "AdministrativeDirector"))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "AdministrativeDirector" });
+                    }
                     return RedirectToAction("Index", "Home");
                 }
             }
@@ -126,60 +130,6 @@ namespace PawMates.Controllers
             await signInManager.SignOutAsync();
 
             return RedirectToAction("Index", "Home");
-        }
-
-
-
-        [Authorize(Roles = "Administrator")]
-        [HttpGet]
-        public async Task<IActionResult> AddRole()
-        {
-            var model = new AddRoleViewModel();
-
-            return View(model);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        public async Task<IActionResult> AddRole(AddRoleViewModel model)
-        {
-
-            if (await roleManager.RoleExistsAsync(model.RoleName)==false)
-            {
-                await roleManager.CreateAsync(userService.CreateRole(model.RoleName));
-            };
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpGet]
-        public async Task<IActionResult> GiveRole()
-        {
-            var model = new UserToRoleViewModel();
-
-            return View(model);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        public async Task<IActionResult> GiveRole(UserToRoleViewModel model)
-        {
-
-            if (await roleManager.RoleExistsAsync(model.RoleName))
-            {
-                var user = await userManager.FindByNameAsync(model.UserName);
-
-
-
-                if (await userManager.IsInRoleAsync(user,model.RoleName)==false 
-                    && user !=null ) 
-                {
-                    await userManager.AddToRoleAsync(user,model.RoleName);
-                }
-            };
-
-            return RedirectToAction("Index", "Home");
-        }
+        }   
     }
 }
